fba54918f306e3144a1390ab273906a3a1c0c57e
Dima Boger <kotvberloge@gmail.com>
2020-04-28 15:58:56 +0300

    :art: Improve checker script

--- tools/check_directory_tree.sh
+++ tools/check_directory_tree.sh
@@ -1,8 +1,14 @@
-check_title() {
+HAS_ERROR=0
+
+check_file() {
   local filepath=$1
   local has_frontmatter_title=0
   local has_ending=0
   local has_beginning=0
+
+  [[ ! $filepath =~ ^(\.?[a-z0-9-]+\/)+(ru|index|en)\.md$ ]] \
+    && echo "$filepath bad_branch_name Check a directory tree, branch should be named only with lowercase letters (a-z), numbers (0-9) and hyphens (-), file should be named as %locale%.md (ru.md, e.g.)" \
+    && HAS_ERROR=1
 
   if grep -q -E '^# .+$' "$filepath"; then
     # has a markdown first level header in file
@@ -29,21 +35,21 @@ check_title() {
 
 
   if [ "$has_ending" = 0 ]; then
-    echo "$filepath open_frontmatter_block Check the .md file, you have open frontmatter block (end it with '---')";
+    echo "$filepath open_frontmatter_block Check the .md file, you have open frontmatter block (end it with '---')"
+    HAS_ERROR=1
+    return
   fi
 
   if [ "$has_frontmatter_title" = 0 ]; then
-    echo "$filepath without_title Check the .md file, it should have frontmatter block with title param or markdown first level header";
+    echo "$filepath without_title Check the .md file, it should have frontmatter block with title param or markdown first level header"
+    HAS_ERROR=1
   fi
 }
 
+IFS=$'\n'; set -f
+for filepath in $(find "$1" -iname '*.md'); do
+    check_file "$filepath"
+done
+unset IFS; set +f
 
-find "$1" -iname '*.md' -print0 |
-  while IFS= read -rd '' filepath;
-  do
-    [[ ! $filepath =~ ^(\.?[a-z0-9-]+\/)+(ru|index|en)\.md$ ]] \
-      && echo "$filepath bad_branch_name Check a directory tree, branch should be named only with lowercase letters (a-z), numbers (0-9) and hyphens (-)";
-
-    check_title "$filepath"
-
-  done
+exit $HAS_ERROR
